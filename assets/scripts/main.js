be55292c97b12802c07f1960cbb2c4e0b1aa4296
Jazmusic1234 <jcc001@ucsd.edu>
2021-02-03 23:16:44 -0800

    Closes #4

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -2,12 +2,13 @@
 
 // TODO
 var volume = document.getElementById("volume-number");
-var aud = document.getElementById("horn-sound")
+var aud = document.getElementById("horn-sound");
 var slider = document.getElementById("volume-slider");
 var icon = document.getElementById("volume-image");
+var image = document.getElementById("sound-image");
 
 // Input field that serves as the textual indicator of sound level
-volume.onchange = function() {
+volume.addEventListener("click", function(event) {
   var volumeValue = volume.value;
   aud.volume = volumeValue/100;
   slider.value = volumeValue;
@@ -20,10 +21,11 @@ volume.onchange = function() {
   } else {
     icon.src = "./assets/media/icons/volume-level-3.svg";
   }
-}
+  event.preventDefault();
+});
 
 // Slider that serves visual controller for sound level
-slider.onchange = function() {
+slider.addEventListener("click", function(event) {
   var sliderValue = slider.value;
   aud.volume = sliderValue/100;
   volume.value = sliderValue;
@@ -36,8 +38,26 @@ slider.onchange = function() {
   } else {
     icon.src = "./assets/media/icons/volume-level-3.svg";
   }
-}
+  event.preventDefault();
+});
 
 // Radio that switches between different horn sounds
+document.getElementById("radio-air-horn").addEventListener("click", function(event) {
+  aud.setAttribute("src", "./assets/media/audio/air-horn.mp3");
+  image.setAttribute("src", "./assets/media/images/air-horn.svg");
+  event.preventDefault();
+});
+
+document.getElementById("radio-car-horn").addEventListener("click", function(event) {
+  aud.setAttribute("src", "./assets/media/audio/car-horn.mp3");
+  image.setAttribute("src", "./assets/media/images/car.svg");
+  event.preventDefault();
+});
+
+document.getElementById("radio-party-horn").addEventListener("click", function(event) {
+  aud.setAttribute("src", "./assets/media/audio/party-horn.mp3");
+  image.setAttribute("src", "./assets/media/images/party-horn.svg");
+  event.preventDefault();
+});
 
 // Button that plays the horn sound
